f4e2d2a7d6c6499980d285b274e09323c4b8a42a
Zobayer <zobayerabedin@gmail.com>
2023-07-05 10:23:44 +0600

    auth

--- app/src/main/java/com/example/cardiac_monitor/logIn.java
+++ app/src/main/java/com/example/cardiac_monitor/logIn.java
@@ -1,20 +1,32 @@
 package com.example.cardiac_monitor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class logIn extends AppCompatActivity {
     Button btmSI, btmSU;
     EditText phn, pass;
+
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        mAuth = FirebaseAuth.getInstance();
 
         phn = findViewById(R.id.phone);
         pass = findViewById(R.id.password);
@@ -35,10 +47,51 @@ public class logIn extends AppCompatActivity {
         btmSU.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(logIn.this, signUp.class);
-                startActivity(intent);
+
             }
         });
+    }
+
+    private void loginUserAccount(String email, String pass)
+    {
+        // validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email!!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(pass)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // signin existing user
+        mAuth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(
+                                    @NonNull Task<AuthResult> task)
+                            {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Login successful!!", Toast.LENGTH_LONG).show();
+                                    // if sign-in is successful
+                                    // intent to home activity
+                                    Intent intent = new Intent(logIn.this, signUp.class);
+                                    startActivity(intent);
+                                }
+
+                                else {
+
+                                    // sign-in failed
+                                    Toast.makeText(getApplicationContext(),
+                                                    "Login failed!!",
+                                                    Toast.LENGTH_LONG)
+                                            .show();
+
+
+                                }
+                            }
+                        });
     }
 
 
